7dde0930955987d1a877a5e5b2a4032f361f6133
TudorHarries <tharries508@gmail.com>
2023-08-10 20:32:49 +0100

    Add about section, improve layout, add placeholder to timeline

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
   return (
     <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="flex-col mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Tudor Harries.</h2>
           <p className="mt-6 text-md leading-8 text-gray-600">
@@ -47,34 +47,39 @@ export default function Home() {
           </div>
         </div>
         
-        <div>
-          
-        </div>
+        <div className="flex justify-between">
+          <div className="w-2/5">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              About me
+            </h3>
+            <p>
+              I am...
+            </p>
+          </div>
 
-        <div>
-          <ol className="relative border-l border-gray-200 dark:border-gray-700">                  
-              <li className="mb-10 ml-4">
-                  <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                  <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-                  <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg className="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg></a>
-              </li>
-              <li className="mb-10 ml-4">
-                  <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                  <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design in Figma</h3>
-                  <p className="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-              </li>
-              <li className="ml-4">
-                  <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                  <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-white">E-Commerce UI code in Tailwind CSS</h3>
-                  <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
-              </li>
-          </ol>
+          <div className="w-3/5">
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">                  
+                <li className="mb-10 ml-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2022-present</time>
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Production Engineer @ Kaluza</h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">My job...</p>
+                </li>
+                <li className="mb-10 ml-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2020-2021</time>
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Engineer (Year in industry) @ Kaluza</h3>
+                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">My job...</p>
+                </li>
+                <li className="ml-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2018-2022</time>
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Computer Science student @ University of Surrey</h3>
+                    <p className="text-base font-normal text-gray-500 dark:text-gray-400">My degree...</p>
+                </li>
+            </ol>
+          </div>
+        
         </div>
       </div>
     </div>
