e3a1476b5e65900d4669ddd1b61a35f17237ceb0
TudorHarries <tharries508@gmail.com>
2023-08-12 10:48:27 +0100

    Start adding links page

---
+++ app/links/page.tsx
@@ -0,0 +1,19 @@
+export default function Links() {
+    return (
+        <div className="bg-white py-24 sm:py-32">
+            <div className="flex-col mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl lg:mx-0">
+                    <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Tudor Harries.</h2>
+                    <p className="mt-4 text-md leading-8 text-gray-600">
+                       Links to things.
+                    </p>
+                </div>
+
+                <div>
+                    <p>link</p>
+                    <p>link</p>
+                </div>
+            </div>
+        </div>
+    )
+}
